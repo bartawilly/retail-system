60c099da0c38b675df03304adeaf14c249c03d06
bartawilly <ahmednasser.bartawilly@gmail.com>
2019-04-11 03:12:39 +0200

    intgrate the template to the app

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+
+//set up template engine
+
+app.set('view engine', 'ejs');
+
+//static files
+
+app.use(express.static('./assets'));
+
+app.get('/', function(req, res) {
+    res.render('dashboard', {active: "dashboard"});
+});
+
+app.get('/icons', function(req, res) {
+    res.render('icons', {active: "icons"});
+});
+
+app.get('/map', function(req, res) {
+    res.render('map', {active: "map"});
+});
+
+app.get('/notifications', function(req, res) {
+    res.render('notifications', {active: "notifications"});
+});
+
+app.get('/tables', function(req, res) {
+    res.render('tables', {active: "tables"});
+});
+
+app.get('/typography', function(req, res) {
+    res.render('typography', {active: "typography"});
+});
+
+app.get('/upgrade', function(req, res) {
+    res.render('upgrade', {active: "upgrade"});
+});
+
+app.get('/user', function(req, res) {
+    res.render('user', {active: "user"});
+});
+
+
+//listen to port 
+
+app.listen(3000);
+
+console.log('you are listening to port 3000');
